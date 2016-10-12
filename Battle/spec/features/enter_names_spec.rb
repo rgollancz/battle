d1c0dabb043320464479cc698737c71ccbddcbf1
Tom Page <tom.w.page@gmail.com>
2016-10-12 14:35:18 +0100

    Completed ex18

--- Battle/spec/features/enter_names_spec.rb
+++ Battle/spec/features/enter_names_spec.rb
@@ -1,6 +1,13 @@
+require 'spec_helper'
 
 feature 'enter_names' do
   scenario 'visiting web page' do
     visit('/')
+    fill_in "name1", :with => "Tom"
+    fill_in "name2", :with => "Anna"
+    click_button "Do a dance"
+    expect(page).to have_text("Tom are yuuuuuu ready!?")
+    expect(page).to have_text("Anna are yuuuuuu ready!?")
+    expect(page).to have_text("Let Battle Commence!")
   end
 end

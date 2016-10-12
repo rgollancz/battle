eb62ce2c1d1b2c2b2d9a419397722e6318d37dde
Anna Lucking <annavlucking@hotmail.co.uk>
2016-10-12 15:07:10 +0100

    add redirect and session

--- Battle/app.rb
+++ Battle/app.rb
@@ -1,19 +1,31 @@
 require 'sinatra/base'
 
-
 class Battle < Sinatra::Base
+
+enable :sessions
+
   get '/' do
+    "name1 =" << session[:name1].inspect
+    "name2 =" << session[:name2].inspect
     erb :index
   end
 
   post '/names' do
+    session[:name1] = params[:name1]
+    session[:name2] = params[:name2]
+    redirect to('/play')
+  end
 
-    @name1 = params[:name1]
-    @name2 = params[:name2]
+  get '/play' do
+    @name1 = session[:name1]
+    @name2 = session[:name2]
     erb :play
   end
 
   run! if app_file == $0
+
 end
+
+
 
 # start the server if ruby file executed directly

c047f88b198223e7ea3d7344703014c54839988c
Anna Lucking <annavlucking@hotmail.co.uk>
2016-10-13 17:33:08 +0100

    game global variable - unfinished

--- app.rb
+++ app.rb
@@ -14,25 +14,24 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    $player_1 = Player.new(params[:name1])
-    $player_2 = Player.new(params[:name2])
+    player_1 = Player.new(params[:name1])
+    player_2 = Player.new(params[:name2])
+    $game = Game.new(player_1, player_2)
     redirect to('/play')
   end
 
   get '/play' do
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    @player_1_hit_points = $player_1.hit_points
-    @player_2_hit_points = $player_2.hit_points
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
+    @player_1_hit_points = $game.player_1.hit_points
+    @player_2_hit_points = $game.player_2.hit_points
     erb :play
   end
 
   get '/attack' do
-    @player_1 = $player_1
-    @player_2 = $player_2
-    @player_1_name = $player_1.name
-    @player_2_name = $player_2.name
-    Game.new.attack(@player_2)
+    @player_1_name = $game.player_1.name
+    @player_2_name = $game.player_2.name
+    $game.attack(@player_2)
     erb :attack
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,8 +1,17 @@
 require 'game.rb'
 
 describe Game do
-  subject(:game) {described_class.new}
-  let(:player) {double :player }
+  subject(:game) {described_class.new("Anna", "Sarah")}
+  #let(:player_1) {double :player_1 }
+  let(:player_2) {double :player_2 }
+
+  describe 'game accepts two player instances' do
+    it 'creates two players' do
+      #it { is_expected.to respond_to(:).with(1).argument }
+      expect(game.player_1).to eq("Anna")
+      expect(game.player_2).to eq("Sarah")
+    end
+  end
 
   describe 'player 1 attacks player 2' do
     it 'responds to attack' do
@@ -11,8 +20,8 @@ describe Game do
     end
 
     it 'responds to receive_damage' do
-      expect(player).to receive(:receive_damage)
-      game.attack(player)
+      expect(player_2).to receive(:receive_damage)
+      game.attack(player_2)
     end
   end
 end
